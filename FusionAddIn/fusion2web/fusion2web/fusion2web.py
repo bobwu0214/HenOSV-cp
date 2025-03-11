2749ce971cc76c6003460d39642b15d6c068453f
Ataraxia-MSJ <694933686@qq.com>
2025-03-11 13:20:29 +0800

    本地完成网页端和fusion端的连接

--- FusionAddIn/fusion2web/fusion2web/fusion2web.py
+++ FusionAddIn/fusion2web/fusion2web/fusion2web.py
@@ -1,4 +1,4 @@
-# Assuming you have not changed the general structure of the template no modification is needed in this file.
+        # Assuming you have not changed the general structure of the template no modification is needed in this file.
 from . import commands
 from .lib import fusionAddInUtils as futil
 import adsk.core, adsk.fusion, adsk.cam, traceback
@@ -19,23 +19,26 @@ _handlers = []  # event handlers
 
 def change_palette(private_info):
     try:
-        # 对网页传来的数据进行处理
-        content = private_info.strip('{}') # 去除首尾的{}
-        pairs = content.split(',') # 分割键值对
-        parameters_ui_list = {}
-        for pair in pairs:
-            key, value = pair.split(':')
-            parameters_ui_list[key] = int(value)
+        # # 对网页传来的数据进行处理
+        # content = private_info.strip('{}') # 去除首尾的{}
+        # pairs = content.split(',') # 分割键值对
+        # parameters_ui_list = {}
+        # for pair in pairs:
+        #     key, value = pair.split(':')
+        #     parameters_ui_list[key] = int(value)
 
         # 修改参数
         design = _app.activeProduct
+        # _ui.messageBox('private_info:{}'.format(private_info))
+        parameters_ui_list = json.loads(private_info)
         for parameter in parameters_ui_list:
             parameter_value = design.userParameters.itemByName(parameter).value #检测目前参数的值
-            if parameter_value == parameters_ui_list[parameter]: # 如果值相等就跳过，不修改参数
+            if parameter_value*10 == parameters_ui_list[parameter]: # 如果值相等就跳过，不修改参数
                 # _ui.messageBox('parameter_value:{}is same'.format(parameter_value))
                 pass
             else:  
-                design.userParameters.itemByName(parameter).expression = str(parameters_ui_list[parameter]*10)
+                design.userParameters.itemByName(parameter).expression = str(parameters_ui_list[parameter])
+                # _ui.messageBox('parameter_value:{}is different'.format(parameter_value))
 
     except:
         futil.handle_error('change_palette')
@@ -48,6 +51,7 @@ class MyOpenedFromURLHandler(adsk.core.WebRequestEventHandler):
     def notify(self, args: adsk.core.WebRequestEventArgs):
         # Get the Private info
         privateInfo = args.privateInfo
+        # _ui.messageBox('privateInfo:{}'.format(privateInfo))
         change_palette(privateInfo) # 修改参数函数
         
         
