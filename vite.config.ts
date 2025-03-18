cf5009840b12d04a16a483932b72ff884f05e907
bobwu0214 <bobwu0214@gmail.com>
2025-03-18 23:12:19 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/HenOSV-cp/',
   plugins: [react()],
-  base: '/HenOSV-cp/',  // 确保路径正确
   build: {
-    outDir: 'docs',  // **这里指定输出到 docs 目录**
-  },
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
