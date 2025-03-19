0070124df17a174fe73372bac378f99d926d9fbe
bobwu0214 <bobwu0214@gmail.com>
2025-03-19 09:40:25 +0800

    test v42

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+  },
+  server: {
+    headers: {
+      "Content-Security-Policy": "default-src 'self'; font-src 'self' data:;"
+    }
   }
 })
