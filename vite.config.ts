c47557635ee5287d21bb894a15d4d78aeec6962f
bobwu0214 <bobwu0214@gmail.com>
2025-03-19 09:57:48 +0800

    testv4

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/HenOSV-cp/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
   server: {
-    headers: {
-      "Content-Security-Policy": "default-src 'self'; font-src 'self' data:;"
+    proxy: {
+      '/api': {
+        target: 'https://static.jianweidata.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
