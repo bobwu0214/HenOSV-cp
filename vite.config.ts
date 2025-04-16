460aec3a8ae193d3398f1665b4fd0423c33b6173
Ataraxia-msj <694933686@qq.com>
2025-04-16 20:13:04 +0800

    ceshi

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',  // 确保 base 为根路径
+  base: './',  // 确保 base 为根路径
   build: {
     outDir: 'docs',  // 输出到 docs 目录
     emptyOutDir: true,  // 构建前清空目录
