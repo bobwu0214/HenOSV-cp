1c045c58fd11da838ecb3b89a8b55ab1923603f5
bobwu0214 <bobwu0214@gmail.com>
2025-05-06 00:47:15 +0800

    更新静态页面构建和资源路径

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Modeling from "./pages/Modeling";
-import Docs from "./pages/Docs";
 import Guide from "./pages/guide";
+import Docs from "./pages/Docs";
+import Navbar from "./components/Navbar";
+import Kit from "./pages/kit";
+import HenOSV from "./pages/henosv";
+import Simulator from "./pages/simulator";
+import SimulatorLab from "./pages/simulatorlab";
+import "./App.css";
 
 function App() {
   return (
@@ -15,6 +20,10 @@ function App() {
         <Route path="/modeling" element={<Modeling />} />
         <Route path="/guide" element={<Guide />} />
         <Route path="/docs" element={<Docs />} />
+        <Route path="/kit" element={<Kit />} />
+        <Route path="/henosv" element={<HenOSV />} />
+        <Route path="/simulator" element={<Simulator />} />
+        <Route path="/simulatorlab" element={<SimulatorLab />} />
       </Routes>
     </Router>
   );
