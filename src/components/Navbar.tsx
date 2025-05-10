6c3148f7b0657385ea0d9d6f7031709e89c6c4f1
bobwu0214 <bobwu0214@gmail.com>
2025-05-10 10:28:42 +0800

    hidden  navbar:无限竞速，文档，计时器

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,15 +8,15 @@ const Navbar = () => {
         <div className="space-x-6 flex justify-end">
           <Link to="/" className="hover:text-gray-400 text-[#4286F3]">首页</Link>
           <Link to="/modeling" className="hover:text-gray-400 text-[#4286F3]">参数化建模</Link>
+          <Link to="/simulatorlab" className="hover:text-gray-400 text-[#4286F3]">仿真套件</Link>
           <Link to="/kit" className="hover:text-gray-400 text-[#4286F3]">教育小车套件</Link>
           <Link to="/henosv" className="hover:text-gray-400 text-[#4286F3]">滑板底盘</Link>
           <Link to="/simulator" className="hover:text-gray-400 text-[#4286F3]">交通工具模拟器</Link>
-          <Link to="/simulatorlab" className="hover:text-gray-400 text-[#4286F3]">仿真套件</Link>
-          <Link to="/guide" className="hover:text-gray-400 text-[#4286F3]">2050-无限竞速</Link>
-          <Link to="/docs" className="hover:text-gray-400 text-[#4286F3]">文档</Link>
+         {/*<Link to="/guide" className="hover:text-gray-400 text-[#4286F3]">2050-无限竞速</Link> */}
+          {/*<Link to="/docs" className="hover:text-gray-400 text-[#4286F3]">文档</Link>*/}
           <Link to="https://profabx.com/prototype/" className="hover:text-gray-400 text-[#4286F3]">数字加工</Link>
           {/* <Link to="https://ataraxia-msj.github.io/jishiqi/" className="hover:text-gray-400 text-[#4286F3]">比赛计时</Link> */}
-          <a href="https://ataraxia-msj.github.io/jishiqi/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400 text-[#4286F3]">比赛计时</a>
+           {/*<a href="https://ataraxia-msj.github.io/jishiqi/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400 text-[#4286F3]">比赛计时</a>*/}
         </div>
       </div>
     </nav>
